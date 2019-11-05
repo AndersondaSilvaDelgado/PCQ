4847daa61c671e5ccc769c1e9920d2cb0cae99ae
andinhodelgado <anderson.sdelgado@gmail.com>
2019-11-05 13:32:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/usinasantafe/pcq/QueimaVegetActivity.java
@@ -0,0 +1,13 @@
+package br.com.usinasantafe.pcq;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QueimaVegetActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_queima_veget);
+    }
+}
---
+++ app/src/main/java/br/com/usinasantafe/pcq/StatusCriterioActivity.java
@@ -0,0 +1,13 @@
+package br.com.usinasantafe.pcq;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StatusCriterioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_criterio);
+    }
+}
